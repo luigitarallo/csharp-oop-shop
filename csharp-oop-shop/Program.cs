c379d70d79e168731de5c3a6be7eba12e9b0420c
luigitarallo <tarallo.gigi@gmail.com>
2024-04-19 17:37:57 +0200

    Add amazzoz array with 4 objects

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -37,7 +37,6 @@
             return $"{Codice} {Nome}";
         }
 
-
         /*-------------------------------------
         Metodo per aggiungere il pad a sinistra
         --------------------------------------*/
@@ -52,12 +51,16 @@
     {
         static void Main(string[] args)
         {
+
+            /*------------------------------------
+            Istanzio un prodotto e provo i metodi
+            -------------------------------------*/
             Prodotto prodotto1 = new Prodotto("Libro Povere Creature", "Romanzo distopico da cui tratto il film", 15, 22);
 
             Console.WriteLine($"Codice prodotto: {prodotto1.Codice}");
             Console.WriteLine($"Nome prodotto: {prodotto1.Nome}");
             Console.WriteLine($"Descrizione prodotto: {prodotto1.Descrizione}");
-            Console.WriteLine($"Il prezzo del prodotto non ivato è {prodotto1.Prezzo} euro");
+            Console.WriteLine($"Il prezzo del prodotto non ivato è {prodotto1.CalcolaPrezzoBase()} euro");
             Console.WriteLine($"L'IVA sul prodotto è {prodotto1.IVA}%");
             Console.WriteLine($"Il prodotto con prezzo ivato è {prodotto1.CalcolaPrezzoConIVA()} euro");
             Console.WriteLine($"Nome esteso: {prodotto1.NomeEsteso()}");
@@ -71,11 +74,30 @@
             Console.WriteLine($"Codice prodotto: {prodotto1.Codice}");
             Console.WriteLine($"Nome prodotto modificato: {prodotto1.Nome}");
             Console.WriteLine($"Descrizione prodotto modificata: {prodotto1.Descrizione}");
-            Console.WriteLine($"Il prezzo modificato del prodotto non ivato è {prodotto1.Prezzo} euro");
+            Console.WriteLine($"Il prezzo modificato del prodotto non ivato è {prodotto1.CalcolaPrezzoBase()} euro");
             Console.WriteLine($"L'IVA modificata sul prodotto è {prodotto1.IVA}%");
             Console.WriteLine($"Il prodotto con prezzo ivato è {prodotto1.CalcolaPrezzoConIVA()} euro");
             Console.WriteLine($"Nome esteso: {prodotto1.NomeEsteso()}");
 
+            /*----------------------------------------
+             Creazione di un Array di Oggetti Prodotto
+             ----------------------------------------*/
+
+            Prodotto[] amazozz = new Prodotto[4];
+
+            amazozz[0] = new Prodotto("Nokia Lumia 920", "Windows Phone", 599, 22);
+            amazozz[1] = new Prodotto("Samsung Galaxy S23", "Android Phone", 599, 22);
+            amazozz[2] = new Prodotto("Apple iPhone 15", "Apple Phone", 1400, 22);
+            amazozz[3] = new Prodotto("Sony PlayStation 5", "Ultima Console di Sony", 449, 22);
+
+            Console.WriteLine("Elenco dei prodotti in amazozz:");
+
+            foreach (Prodotto prodotto in amazozz)
+            {
+                Console.WriteLine($"Codice: {prodotto.CodicePadLeft()}, Nome: {prodotto.Nome}, Prezzo senza IVA: {prodotto.CalcolaPrezzoBase()} euro");
+                Console.WriteLine($"Descrizione prodotto: {prodotto.Descrizione}");
+                Console.WriteLine($"Prezzo con IVA: {prodotto.CalcolaPrezzoConIVA()} euro");
+            }
         }
     }
 }
